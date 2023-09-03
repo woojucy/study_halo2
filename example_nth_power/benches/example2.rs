e4714903d8b7ea29ac252a382b083b9ee106b4f7
woojucy <woojucy2@gmail.com>
2023-09-03 22:26:53 +0900

    Add bench-mark which stores params and a proof

--- example_nth_power/benches/example2.rs
+++ example_nth_power/benches/example2.rs
@@ -1,14 +1,25 @@
-use halo2_proofs::{
-    arithmetic::FieldExt,
-    circuit::*,
-    pasta::{vesta, Fp},
-    plonk::*,
+use halo2::{circuit::*, halo2curves::ff::PrimeField, plonk::*, poly::Rotation};
+use halo2::{
+    halo2curves,
     poly::commitment::Params,
-    poly::Rotation,
+    poly::{
+        commitment::ParamsProver,
+        ipa::{
+            commitment::{IPACommitmentScheme, ParamsIPA},
+            multiopen::{ProverIPA, VerifierIPA},
+            strategy::AccumulatorStrategy,
+        },
+    },
     transcript::{Blake2bRead, Blake2bWrite, Challenge255},
+    transcript::{TranscriptReadBuffer, TranscriptWriterBuffer},
 };
-use std::marker::PhantomData;
-
+use halo2curves::pasta::{EqAffine, Fp};
+use std::{
+    fs::File,
+    io::{BufReader, Read, Write},
+    marker::PhantomData,
+    path::Path,
+};
 // bench-mark tool
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::rngs::OsRng;
@@ -25,12 +36,12 @@ struct PowerByNumConfig {
 }
 
 #[derive(Debug, Clone)]
-struct PowerByNumChip<F: FieldExt> {
+struct PowerByNumChip<F: PrimeField> {
     config: PowerByNumConfig,
     _marker: PhantomData<F>,
 }
 
-impl<F: FieldExt> PowerByNumChip<F> {
+impl<F: PrimeField> PowerByNumChip<F> {
     pub fn construct(config: PowerByNumConfig) -> Self {
         Self {
             config,
@@ -147,7 +158,7 @@ impl<F: FieldExt> PowerByNumChip<F> {
 #[derive(Default, Clone, Copy)]
 struct TestCircuit<F>(PhantomData<F>);
 
-impl<F: FieldExt> Circuit<F> for TestCircuit<F> {
+impl<F: PrimeField> Circuit<F> for TestCircuit<F> {
     type Config = PowerByNumConfig;
     type FloorPlanner = SimpleFloorPlanner;
 
@@ -187,8 +198,22 @@ impl<F: FieldExt> Circuit<F> for TestCircuit<F> {
 const K: u32 = 7;
 
 fn bench_example(name: &str, c: &mut Criterion) {
-    let mut rng = OsRng;
-    let params: Params<vesta::Affine> = Params::new(K);
+    // Initialize the polynomial commitment parameters
+    let params_path = Path::new("./benches/params/example2_params");
+    if File::open(params_path).is_err() {
+        // let params: ParamsIPA<EqAffine> = ParamsIPA::new(k);
+        let params: ParamsIPA<EqAffine> = ParamsIPA::new(K);
+        let mut buf = Vec::new();
+
+        params.write(&mut buf).expect("Failed to write params");
+        let mut file = File::create(params_path).expect("Failed to create params");
+
+        file.write_all(&buf[..])
+            .expect("Failed to write params to file");
+    }
+    let params_fs = File::open(params_path).expect("Failed to load params");
+    let params: ParamsIPA<EqAffine> =
+        ParamsIPA::read::<_>(&mut BufReader::new(params_fs)).expect("Failed to read params");
 
     let empty_circuit = TestCircuit(PhantomData);
     let vk = keygen_vk(&params, &empty_circuit).expect("vk generation failed");
@@ -206,51 +231,108 @@ fn bench_example(name: &str, c: &mut Criterion) {
 
     let circuit = TestCircuit(PhantomData);
 
-    c.bench_function(&prover_name, |b| {
-        b.iter(|| {
-            let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
-            create_proof(
-                &params,
-                &pk,
-                &[circuit],
-                &[&[&public_input]],
-                &mut rng,
-                &mut transcript,
-            )
-            .expect("proof generation failed")
-        })
-    });
+    // Benchmark proof creation
+    // c.bench_function(&prover_name, |b| {
+    //     b.iter(|| {
+    //         let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+    //         create_proof::<IPACommitmentScheme<_>, ProverIPA<_>, _, _, _, _>(
+    //             &params,
+    //             &pk,
+    //             &[circuit],
+    //             &[&[&public_input]],
+    //             OsRng,
+    //             &mut transcript,
+    //         )
+    //         .expect("proof generation failed");
+    //     });
+    // });
 
-    let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
-    create_proof(
-        &params,
-        &pk,
-        &[circuit],
-        &[&[&public_input]],
-        &mut rng,
-        &mut transcript,
-    )
-    .expect("proof generation failed");
-    let proof = transcript.finalize();
+    // Create a proof
+    let proof_path = Path::new("./benches/proofs/example2_proof");
+    if File::open(proof_path).is_err() {
+        let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+        create_proof::<IPACommitmentScheme<_>, ProverIPA<_>, _, _, _, _>(
+            &params,
+            &pk,
+            &[circuit],
+            &[&[&public_input]],
+            OsRng,
+            &mut transcript,
+        )
+        .expect("proof generation failed");
+        let proof: Vec<u8> = transcript.finalize();
+        let mut file = File::create(proof_path).expect("Failed to create proof");
+        file.write_all(&proof[..]).expect("Failed to write proof");
+    }
+
+    let mut proof_fs = File::open(proof_path).expect("Failed to load proof");
+    let mut proof = Vec::<u8>::new();
+    proof_fs
+        .read_to_end(&mut proof)
+        .expect("Couldn't read proof");
 
     c.bench_function(&verifier_name, |b| {
         b.iter(|| {
-            let strategy = SingleVerifier::new(&params);
+            use halo2::poly::VerificationStrategy;
+            let strategy = AccumulatorStrategy::new(&params);
             let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&proof[..]);
-            assert!(verify_proof(
+            let strategy = verify_proof::<IPACommitmentScheme<_>, VerifierIPA<_>, _, _, _>(
                 &params,
                 pk.get_vk(),
                 strategy,
                 &[&[&public_input]],
-                &mut transcript
+                &mut transcript,
             )
-            .is_ok());
+            .unwrap();
+            assert!(strategy.finalize());
         });
     });
+
+    // c.bench_function(&prover_name, |b| {
+    //     b.iter(|| {
+    //         let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+    //         create_proof(
+    //             &params,
+    //             &pk,
+    //             &[circuit],
+    //             &[&[&public_input]],
+    //             &mut rng,
+    //             &mut transcript,
+    //         )
+    //         .expect("proof generation failed")
+    //     })
+    // });
+
+    // let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+    // create_proof(
+    //     &params,
+    //     &pk,
+    //     &[circuit],
+    //     &[&[&public_input]],
+    //     &mut rng,
+    //     &mut transcript,
+    // )
+    // .expect("proof generation failed");
+    // let proof = transcript.finalize();
+
+    // c.bench_function(&verifier_name, |b| {
+    //     b.iter(|| {
+    //         let strategy = SingleVerifier::new(&params);
+    //         let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&proof[..]);
+    //         assert!(verify_proof(
+    //             &params,
+    //             pk.get_vk(),
+    //             strategy,
+    //             &[&[&public_input]],
+    //             &mut transcript
+    //         )
+    //         .is_ok());
+    //     });
+    // });
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    bench_example("example1", c);
+    bench_example("example2", c);
 }
 
 criterion_group!(benches, criterion_benchmark);
